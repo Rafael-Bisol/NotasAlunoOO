a92a78e4ceed536a20314cb003ada539d2f4b7d1
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-02-28 19:42:43 -0300

    Projeto Criado

---
+++ NotasAlunoOO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotasAlunoOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
