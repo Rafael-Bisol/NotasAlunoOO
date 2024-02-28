701700311d11c22a0654c517a6e96c5eef3f5f0c
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-02-28 19:47:55 -0300

    Adicionada classe Aluno

---
+++ NotasAlunoOO/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NotasAlunoOO
+{
+    public class Aluno
+    {
+        public int nota1, nota2, nota3;
+
+        public int Media()
+        {
+            return (nota1 * 30 + nota2 * 35 + nota3 * 35) / 100;
+        }
+    }
+}
