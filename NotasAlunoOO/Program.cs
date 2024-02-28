73284f6501c19e62fd630f37a17e44dad555bceb
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-02-28 19:54:06 -0300

    Resolvido com OO

--- NotasAlunoOO/Program.cs
+++ NotasAlunoOO/Program.cs
@@ -6,27 +6,28 @@ namespace NotasAlunoOO
     {
         static void Main(string[] args)
         {
+            Aluno aluno = new Aluno();
+
             Console.WriteLine("Notas do Aluno");
             Console.Write("Nome do Aluno: ");
-            string nome = Console.ReadLine();
+            aluno.nome = Console.ReadLine();
             Console.Write("Nota do primeiro trimestre: ");
-            int primeiroTrimestre = int.Parse(Console.ReadLine());
+            aluno.nota1 = int.Parse(Console.ReadLine());
             Console.Write("Nota do segundo trimestre: ");
-            int segundoTrimestre = int.Parse(Console.ReadLine());
+            aluno.nota2 = int.Parse(Console.ReadLine());
             Console.Write("Nota do terceiro trimestre: ");
-            int terceiroTrimestre = int.Parse(Console.ReadLine());
+            aluno.nota3 = int.Parse(Console.ReadLine());
 
-            int media = (primeiroTrimestre * 30 + segundoTrimestre * 35 + terceiroTrimestre * 35) / 100;
-            Console.WriteLine($"Aluno {nome} tirou {media}");
+            Console.WriteLine($"Aluno {aluno.nome} tirou {aluno.Media()}");
 
-            if (media >= 60)
+            if (aluno.Media() >= 60)
             {
                 Console.WriteLine("APROVADO");
             }
             else
             {
                 Console.WriteLine("REPROVADO");
-                Console.WriteLine("Faltam {0} para atingir 60 pontos", 60 - media);
+                Console.WriteLine("Faltam {0} para atingir 60 pontos", 60 - aluno.Media());
             }
         }
     }
